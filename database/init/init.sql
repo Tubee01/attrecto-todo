d949c36fed64e1fed2897f5e79e732709abc6ec6
Tubee <Tubee01@users.noreply.github.com>
2022-08-20 11:05:22 +0200

    feat fix (Docker): init sql , shared volumes

--- database/init/init.sql
+++ database/init/init.sql
@@ -2,25 +2,25 @@ DROP DATABASE IF EXISTS master;
 
 CREATE DATABASE master;
 
-\ c master;
+\c master;
 
-CREATE TABLE User (
-    id uuid DEFAULT uuid_generate_v4() NOT NULL,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    created_at timestamp DEFAULT current_timestamp NOT NULL,
-    updated_at timestamp DEFAULT current_timestamp NOT NULL,
-    CONSTRAINT pk_user PRIMARY KEY (id) admin boolean DEFAULT false NOT NULL
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "name" varchar(255) NOT NULL,
+    "email" varchar(255) NOT NULL,
+    "password" varchar(255) NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT "pk_user" PRIMARY KEY ("id")
 );
 
-CREATE TABLE Todo (
-    id uuid DEFAULT uuid_generate_v4() NOT NULL,
-    name varchar(255) NOT NULL,
-    description varchar(500),
-    user_id uuid NOT NULL,
-    created_at timestamp DEFAULT current_timestamp NOT NULL,
-    updated_at timestamp DEFAULT current_timestamp NOT NULL,
-    CONSTRAINT pk_todo PRIMARY KEY (id),
-    CONSTRAINT fk_todo_user FOREIGN KEY (user_id) REFERENCES User (id) ON DELETE CASCADE
+CREATE TABLE "Todo"(
+    "id" UUID NOT NULL,
+    "name" varchar(255) NOT NULL,
+    "description" varchar(500),
+    "user_id" uuid NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT "pk_todo" PRIMARY KEY ("id"),
+    CONSTRAINT "fk_todo_user" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE CASCADE
 );
