aa86b6f0a5d6d6afb9f60783d3ef137342b03edf
Tubee01 <tamasi.bence@outlook.com>
2022-08-26 11:16:41 +0200

    feat (Backend) : origin '*'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,10 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       credentials: true,
-      origin: [
-        'http://localhost:3000',
-        'http://192.168.1.180:3000',
-      ],
+      origin: '*'
     },
   });
   // enable cors requests
