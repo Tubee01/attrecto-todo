ecc471aa0d6fef32030ee5b73e3a0da9272d24fc
Tubee01 <tamasi.bence@outlook.com>
2022-08-26 17:19:20 +0200

    Update endpoints.module.ts

---
+++ backend/src/endpoints/endpoints.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
+
+@Module({
+  imports: [UserModule, AuthModule, TodoModule],
+})
+export class EndpointsModule {}
